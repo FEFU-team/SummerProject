498733405541b605e63fc193de9faa78cd205a94
vladStar223 <vlad32kolm@gmail.com>
2025-04-07 09:03:53 +1000

    add

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,40 @@
 #include <SFML/Graphics.hpp>
 int main()
+
 {
+//#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+//#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
-
-    window.setPosition({ 100, 100 });
-
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color(150, 50, 250));
+    shape.setPosition({200,400});
+    // define a circle with radius = 200
+    sf::CircleShape circle(200.f);
+    sf::RectangleShape rec({100,100});
+    // change the radius to 40
+    circle.setRadius(100.f);
+    sf::Vector2f p = { 100,100 };
+    circle.setPosition(p);
+    
+    // change the number of sides (points) to 100
+    circle.setPointCount(50);
+    
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+            
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
             }
         }
+        
         window.clear();
+        window.draw(shape);
+        window.draw(circle);
+        window.draw(rec);
         window.display();
     }
 }
