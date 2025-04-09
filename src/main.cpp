897617a3b56553342f8c5afda00f0c58d3e33d5b
vladStar223 <vlad32kolm@gmail.com>
2025-04-09 11:59:06 +1000

    add move shape

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,36 @@ int main()
 //#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
-    
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
-            
-           
             if (event->is<sf::Event::Closed>())
             {
                 cout << "d" << endl;
                 window.close();
             }
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::W) {
+                    shape.move({ 0,-10 });
+                }
+                else if (keyPressed->scancode == sf::Keyboard::Scancode::D) {
+                    shape.move({ 10,0 });
+                }
+                else if (keyPressed->scancode == sf::Keyboard::Scancode::S) {
+                    shape.move({ 0,10 });
+                }
+                else if (keyPressed->scancode == sf::Keyboard::Scancode::A) {
+                    shape.move({ -10,0});
+                }
+                    
+            }
         }
-        
         window.clear();
-       
+        window.draw(shape);
         window.display();
     }
 }
