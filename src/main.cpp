104a3b2046f111498c401ee550c33787b76da09c
vladStar223 <vlad32kolm@gmail.com>
2025-04-13 00:55:47 +1000

    test5

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,27 @@
 
 #include <SFML/Graphics.hpp>
 
-int main(){
-    // #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
-    // #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+int main() {
 
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    // #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+    // #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
     sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color(150, 50, 250));
-    shape.setPosition({200, 400});
-    // define a circle with radius = 200
-    sf::CircleShape circle(200.f);
-    sf::RectangleShape rec({100, 100});
-    // change the radius to 40
-    circle.setRadius(100.f);
-    sf::Vector2f p = { 100, 100 };
-    circle.setPosition(p);
-    double v  = .5;
-    int x_xcxcxc_122;
-    for (int i = 0; i <= 0; i++) {
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(300.f, 200.f);
 
-    }
-    // change the number of sides (points) to 100
-    circle.setPointCount(50);
-    
     while (window.isOpen()) {
-        while (const std::optional event = window.pollEvent()) {
-            
-            if (event->is<sf::Event::Closed>()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
         }
-        
+
         window.clear();
         window.draw(shape);
-        window.draw(circle);
-        window.draw(rec);
         window.display();
     }
+
+    return 0;
 }
