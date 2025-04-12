845ad81caae9456c104713833b45601a4928ce92
vladStar223 <vlad32kolm@gmail.com>
2025-04-13 01:18:46 +1000

    test11

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,23 @@
 
 #include <SFML/Graphics.hpp>
 
-int
-main ()
+int main()
 {
-    auto window = sf::RenderWindow (sf::VideoMode ({ 1920u, 1080u }),
+    auto window = sf::RenderWindow(sf::VideoMode ({ 1920u, 1080u }),
                                     "CMake SFML Project");
-    window.setFramerateLimit (144);
+    window.setFramerateLimit(144);
 
-    sf::CircleShape shape (50.f);
-    shape.setFillColor (sf::Color (150, 50, 250));
-    shape.setPosition ({ 200, 400 });
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color (150, 50, 250));
+    shape.setPosition({ 200, 400 });
 
-    sf::CircleShape circle (200.f);
-    sf::RectangleShape rec ({ 100, 100 });
-    circle.setRadius (100.f);
+    sf::CircleShape circle(200.f);
+    sf::RectangleShape rec({ 100, 100 });
+    circle.setRadius(100.f);
 
     sf::Vector2f p = { 100, 100 };
-    circle.setPosition (p);
-    circle.setPointCount (50);
+    circle.setPosition(p);
+    circle.setPointCount(50);
 
     double velocity = 0.5;
     int unused_variable; //  Две пробела перед комментарием
@@ -31,11 +30,11 @@ main ()
             }
         }
 
-        window.clear ();
-        window.draw (shape);
-        window.draw (circle);
-        window.draw (rec);
-        window.display ();
+        window.clear();
+        window.draw(shape);
+        window.draw(circle);
+        window.draw(rec);
+        window.display();
     }
 
     return 0;
