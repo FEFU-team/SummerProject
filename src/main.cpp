6c39c22cc994a979308c8a38922ef845e533bba0
vladStar223 <vlad32kolm@gmail.com>
2025-04-12 23:43:39 +1000

    test 2

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ int main()
     sf::Vector2f p = { 100,100 };
     circle.setPosition(p);
     double v  = .5;
+    int x;
     // change the number of sides (points) to 100
     circle.setPointCount(50);
     
