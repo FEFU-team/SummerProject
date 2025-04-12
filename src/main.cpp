3600f6648d655ed65af71f4a698db2356be86851
vladStar223 <vlad32kolm@gmail.com>
2025-04-13 01:01:55 +1000

    test6

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,40 @@
-
 #include <SFML/Graphics.hpp>
 
-int main() {
-
+int main ()
+{
     // #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
     // #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
-    sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition(300.f, 200.f);
+    auto window = sf::RenderWindow (sf::VideoMode ({ 1920u, 1080u }),
+                                    "CMake SFML Project");
+    window.setFramerateLimit (144);
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
+    sf::CircleShape shape (50.f);
+    shape.setFillColor (sf::Color (150, 50, 250));
+    shape.setPosition ({ 200, 400 });
+
+    sf::CircleShape circle (200.f);
+    sf::RectangleShape rec ({ 100, 100 });
+    circle.setRadius (100.f);
+
+    sf::Vector2f p = { 100, 100 };
+    circle.setPosition (p);
+    circle.setPointCount (50);
+
+    double velocity = 0.5;
+    int unused_variable; // Переименовано для ясности
+
+    while (window.isOpen ()) {
+        while (const std::optional event = window.pollEvent ()) {
+            if (event->is<sf::Event::Closed> ()) {
+                window.close ();
             }
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        window.clear ();
+        window.draw (shape);
+        window.draw (circle);
+        window.draw (rec);
+        window.display ();
     }
 
     return 0;
