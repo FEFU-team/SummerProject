8602ff0c0d0fbafa4cd90cb96158cdd1e17da1d6
vladStar223 <vlad32kolm@gmail.com>
2025-05-01 15:39:34 +1000

    bug with event

--- src/engine/Engine.h
+++ src/engine/Engine.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "D:\SFML\SFML_TEST\src\Main.hpp"
+#define const unsigned int HEIG2HT_WINDOW = 1920;//размер высоты окна
+#define const unsigned  int WID2TH_WINDOW = 1080;//размер  ширины окна 
 class Engine
 {
 protected:
-	sf::RenderWindow* window;
+	sf::RenderWindow* window = nullptr;
 public:
 	void Init();
 	void Run();
-
 	void End();
 	Engine();
 	~Engine();
