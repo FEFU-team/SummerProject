496f04372873f5439a72caf7d1eebb6c74c2b1ab
vladStar223 <vlad32kolm@gmail.com>
2025-05-02 13:38:11 +1000

    add fixfix for

--- include/UI/Map/Map.h
+++ include/UI/Map/Map.h
@@ -8,6 +8,7 @@ private:
 	 const  int HEIGHT = 10;
 	 const  int WIDTH = 10;
 public:
+	
 	void Draw(sf::RenderTarget* window) override;
 	Map(sf::Vector2f positions = {0,0});
 	std::vector<std::vector<Cell>> grid;
--- src/UI/Map/Map.cpp
+++ src/UI/Map/Map.cpp
@@ -15,13 +15,13 @@ Map::Map(sf::Vector2f positions ) {
 	position = positions;
 	int size_h = HEIGHT;
 	int size_w = WIDTH;
-	for (int i = position.x; int h = 0; h < size_h; i++) {
-		float posX = (50+1) * i;
+	for (int i = position.x, h = 0; h < size_h; i++, h++) {
+		float posX = (50 + 1) * i;
 		vector<Cell> line;
-		for (int j = position.y; int w = 0; w < size_w; j++; w++) {
+		for (int j = position.y, w = 0; w < size_w; j++, w++) {
 			float posY = (50 + 1) * j;
-			line.push_back(Cell({ posX,posY }));
+			line.push_back(Cell({ posX, posY }));
 		}
 		grid.push_back(line);
-    }
+	}
 }
