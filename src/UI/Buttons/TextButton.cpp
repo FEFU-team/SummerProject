39888b6272fc1e54fa3a08835323357d592c78ef
vladStar223 <vlad32kolm@gmail.com>
2025-05-18 23:04:27 +1000

    fix all

--- src/UI/Buttons/TextButton.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/UI//Buttons/Button.h"
-#include<iostream>
-#include "../include/UI//Buttons/TextButton.h"
-TextButton::TextButton(sf::Vector2f position, sf::Color color) :Button(position, { 100,100 }, color)
-{
-	
-	
-}
