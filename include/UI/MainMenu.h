02b3123e9c2178daaba62655f2bffe0544efa800
vladStar223 <vlad32kolm@gmail.com>
2025-05-21 22:53:31 +1000

    fix

--- include/UI/MainMenu.h
+++ include/UI/MainMenu.h
@@ -4,5 +4,7 @@
 #include<vector>
 class MainMenu:public Element {
 public:
+	void draw(sf::RenderTarget* window) override;
+
 	void hello();
 };
--- src/UI/Menu/MainMenu.cpp
+++ src/UI/Menu/MainMenu.cpp
@@ -1,5 +1,8 @@
 #include "..\include\UI\MainMenu.h"
 #include <iostream>
+void MainMenu::draw(sf::RenderTarget* window) {
+
+}
 void MainMenu::hello()
 {
 	std::cout << "dd" << std::endl;
