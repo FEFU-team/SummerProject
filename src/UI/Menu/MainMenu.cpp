85741b22ed157068e2a3085a332767a643bbbf7b
vladStar223 <vlad32kolm@gmail.com>
2025-05-27 14:38:19 +1000

    fix add conctructor

--- src/UI/Menu/MainMenu.cpp
+++ src/UI/Menu/MainMenu.cpp
@@ -4,15 +4,34 @@
 
 #include <memory>
 #include <UI/Map/Cell.h>
-MainMenu::MainMenu(sf::Vector2f position, int height, int width, sf::Font font, GameController* game_controller_ptr) {
+MainMenu::MainMenu(sf::Vector2f position, int height, int width,  sf::Font& font, GameController* game_controller_ptr) {
 	this->position = position;
 	this->game_controller_ptr = game_controller_ptr;
+	
+	
 	widgets.push_back(
-		std::make_unique<TextButton>(sf::Vector2f((height / 2) -100, (width / 2) -200), L"Шашки", sf::Vector2f(200, 100),sf::Color::Black, sf::Color::White,60));
+		std::make_unique<TextButton>(sf::Vector2f((height / 2) - 100, (width / 2) - 200), L"Игра", font));
+	widgets.push_back(
+		std::make_unique<TextButton>(
+			sf::Vector2f((height / 2) - 100, (width / 2) - 100),
+			L"Играть", font,30,sf::Vector2f(200,100),
+			sf::Color::White,
+			sf::Color::Black));
+	widgets.push_back(
+		std::make_unique<TextButton>(
+			sf::Vector2f((height / 2) - 100, (width / 2) + 50),
+			L"Выход", font, 30, sf::Vector2f(200, 100),
+			sf::Color::White,
+			sf::Color::Black));
+			
+	/*
+	widgets.push_back(
+		std::make_unique<TextButton>(sf::Vector2f((height / 2) -100, (width / 2) -200), L"Шашки", sf::Vector2f(200, 100),sf::Color::Black, sf::Color::White,60,font));
 	widgets.push_back(
 		std::make_unique<TextButton>(sf::Vector2f((height / 2) - 100, (width / 2) - 100), L"Играть", sf::Vector2f(200, 100)));
 	widgets.push_back(
 		std::make_unique<TextButton>(sf::Vector2f((height / 2) -100, (width / 2) + 50), L"Выход", sf::Vector2f(200, 100)));
+		*/
 
 }
 void MainMenu::update_input(sf::Vector2f mouse_position) {
