195e4736351b745c26b0239ce9f955ff85417050
vladStar223 <vlad32kolm@gmail.com>
2025-06-19 12:08:29 +1000

    fix all

--- include/UI/Checkers/King.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include <UI//Checkers/Checker.h>
-#include <iostream>
-class King :public Checker {
-public:
-	King(sf::Vector2f position, sf::Color color, sf::Texture* texture);
-};
